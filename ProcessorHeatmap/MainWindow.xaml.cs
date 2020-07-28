1705d6bb9101f91b1975b887bbb09fc826caba83
tarpaha <ppopchikovsky@gmail.com>
2020-07-29 00:56:52 +0300

    window dimension calculation was extracted to separate class

--- ProcessorHeatmap/MainWindow.xaml.cs
+++ ProcessorHeatmap/MainWindow.xaml.cs
@@ -37,14 +37,7 @@ namespace ProcessorHeatmap
 
             _cpuUsage = new CpuUsage();
 
-            // work out how many columns and rows we need (closest square values)
-            double idealWidthAndHeight = Math.Sqrt(_cpuUsage.CoreCount);
-            int cols = (int)Math.Truncate(idealWidthAndHeight);
-            if (cols < idealWidthAndHeight)
-            {
-                cols++;
-            }
-            int rows = (int)Math.Truncate(idealWidthAndHeight);
+            var (cols, rows) = Utils.CalculateDimensions(_cpuUsage.CoreCount);
 
             _rowCount = rows;
             _colCount = cols;
