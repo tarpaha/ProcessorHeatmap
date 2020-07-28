30c85fd003f7907e135eae89e85b616dd8028ebd
tarpaha <ppopchikovsky@gmail.com>
2020-07-29 00:56:52 +0300

    window dimension calculation was extracted to separate class
    (cherry picked from commit 1705d6bb9101f91b1975b887bbb09fc826caba83)

---
+++ ProcessorHeatmap/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProcessorHeatmap
+{
+    public static class Utils
+    {
+        public static (int, int) CalculateDimensions(int cellsCount)
+        {
+            // work out how many columns and rows we need (closest square values)
+            double idealWidthAndHeight = Math.Sqrt(cellsCount);
+            int cols = (int)Math.Truncate(idealWidthAndHeight);
+            if (cols < idealWidthAndHeight)
+            {
+                cols++;
+            }
+            int rows = (int)Math.Truncate(idealWidthAndHeight);
+            return (cols, rows);
+        }
+    }
+}
